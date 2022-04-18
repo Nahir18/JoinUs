1136d2a124d73845fc50f836f80cf4f2c170af09
Antonov <alexander.antonov@publicismedia.com>
2022-04-18 21:43:54 +0500

    Login page.

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,9 +1,11 @@
-import React, {useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import PropTypes from 'prop-types';
 import Form from "@Components/Forms/index"
 import Button from "@Components/Button";
 import Input from "@Components/Fields/Input"
+import axios from "axios";
 import { WithValidationHocRenderPropAdapter} from "../../Validator";
+import {DEFAULT_URL_FOR_FILE} from "../../components/APIList";
 
 
 const loginFields = [
@@ -22,27 +24,37 @@ const loginFields = [
         formColumn: 0,
     }
 ]
+const rules = {
+    email: "required",
+    password: "required"
+}
 
 const Login = props => {
     const [data, setData] = useState({})
     const [loading, setLoading] = useState(false)
+    const inputLoginFields = (value) => setData(value)
+    const authorize = () => {
+        axios.post(`${DEFAULT_URL_FOR_FILE}/auth/employee/`,  data, {headers: {'Content-type': 'application/json'}})
+    }
     return (
         <div
             className="flex justify-center items-center flex-col w-full bg-color-light-blue-2"
         >
             <WithValidationHocRenderPropAdapter
-                onInput={() => console.log("oninput")}
-                onSubmit={() => console.log("onsubmit")}
+                onInput={inputLoginFields}
+                onSubmit={authorize}
                 value={data}
+                rules={rules}
             >
                 {(formProps) => {
                     const {formValid, onSubmit, onInput } = formProps
                     return (
                         <>
                             <Form
+                                {...formProps}
                                 fields={loginFields}
                                 value={data}
-                                onInput={() => onInput}
+                                onInput={onInput}
                             />
                             <Button
                                 className="p-t-14"
